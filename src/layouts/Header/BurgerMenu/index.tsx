6010589e47c0df0b9baa34764b9241f3360bbbc1
Belka <belka.srh.it@gmail.com>
2024-01-24 08:37:47 +0100

    update: styles

--- src/layouts/Header/BurgerMenu/index.tsx
+++ src/layouts/Header/BurgerMenu/index.tsx
@@ -14,25 +14,27 @@ interface IBurgerMenyProps {
 
 const BurgerMenu = ({ isOpen, setIsOpen }: IBurgerMenyProps) => {
   return (
-    <div className={classNames(s.burger, isOpen && s['is-open'])}>
-      <button
-        className={s.button}
-        type="button"
-        onClick={() => setIsOpen(false)}
-      >
-        <SvgIcon id="ui-cross" width={40} height={40} />
-      </button>
+    <div className={classNames(s.backdrop, isOpen && s['is-open'])}>
+      <div className={s.burger}>
+        <button
+          className={s.button}
+          type="button"
+          onClick={() => setIsOpen(false)}
+        >
+          <SvgIcon id="ui-cross" width={40} height={40} />
+        </button>
 
-      <div onClick={() => setIsOpen(false)}>
-        <SiteNav styleNav={s.nav} styleLink={s.nav__link} />
+        <div onClick={() => setIsOpen(false)}>
+          <SiteNav styleNav={s.nav} styleLink={s.nav__link} />
+        </div>
+        <ContactInfo styleInfo={s.contacts} styleLink={s.contacts__link} />
+
+        <Socials
+          variant="title"
+          styleSocials={s.socials}
+          styleLink={s.socials__link}
+        />
       </div>
-      <ContactInfo styleInfo={s.contacts} styleLink={s.contacts__link} />
-
-      <Socials
-        variant="title"
-        styleSocials={s.socials}
-        styleLink={s.socials__link}
-      />
     </div>
   );
 };
